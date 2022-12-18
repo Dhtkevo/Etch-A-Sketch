08a4e44f21afbd2630864a431ade4f41402d7e60
Kevin <110263079+Dhtkevo@users.noreply.github.com>
2022-12-18 11:52:38 -0500

    Hover over grid blocks

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,19 @@
 const container = document.querySelector('.container');
+const allGridBlocks = document.querySelectorAll('.grid-block');
+
 
 for (i = 0; i < 32; i++) {
     const block = document.createElement('div');
     block.classList.add('grid-block');
+    block.classList.add('change-color');
     container.appendChild(block);
+
+}
+
+const hoverOverBlocks = () => {
+    allGridBlocks.forEach(function(block) {
+        block.addEventListener('mouseover', function () {
+            block.classList.add('change-color');
+        });
+    });
 }
